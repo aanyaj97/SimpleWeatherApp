d21d1ec1adcb47b2aa5691e7201312d89c3ca7ab
Aanya Jhaveri <Aanya@aanyas-laptop.local>
2017-08-04 19:13:19 -0500

    Background Color Changes according to temperature, city label

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -13,17 +13,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var forecastLabel: UILabel!
     
+    @IBOutlet weak var cityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let manager = AFHTTPSessionManager()
-        manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=c51b743bface06830ba461e8597a0702",
+        manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Chicago&mode=json&units=metric&cnt=1&appid=c51b743bface06830ba461e8597a0702",
                     parameters: nil,
                     progress: nil,
                     success: {(operation: URLSessionDataTask, responseObject: Any?) in
                         let json = JSON(responseObject)
                         if let forecast = json["list"][0]["weather"][0]["description"].string {
-                            self.forecastLabel.text = forecast
+                            self.forecastLabel.text = forecast }
+                        if let temperature = json["list"][0]["temp"]["day"].float {
+                            let farenheit = temperature * (9/5) + 32
+                            if farenheit > 50 {
+                                self.view.backgroundColor = UIColor.red
+                            } else if farenheit <= 50 {
+                                self.view.backgroundColor = UIColor.blue
+                            }
+                        }
+                        if let city = json["city"]["name"].string {
+                            self.cityLabel.text = city
                         }
         })      { (operation:URLSessionDataTask?, error:Error) in
                         print("Error: " + error.localizedDescription)
@@ -39,3 +50,5 @@ class ViewController: UIViewController {
 }
 
 
+//QUESTION: I installed using Carthage and the Cartfile. But it wasn't recognizing JSON? 
+//TO DO: Use JSON to display which city you are looking at (maybe drop down menu????)
